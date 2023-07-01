6245e768a95196fc602ae36724d48c9f78ee8331
bibin k b <bibinkochumalayil007@gmail.com>
2023-07-01 19:44:59 +0530

    first commit

---
+++ lib/presentation/downloads/screen_downloads.dart
@@ -0,0 +1,214 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:netflix/application/downloads/downloads_bloc.dart';
+import 'package:netflix/core/colors.dart';
+import 'package:netflix/core/constants.dart';
+import 'package:netflix/presentation/widgets/app_bar_widget.dart';
+
+class ScreeDownloads extends StatelessWidget {
+  ScreeDownloads({super.key});
+
+  final _widgetList = [
+    const _SmartDownloads(),
+    Section2(),
+    const Section3(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: AppBarWidget(title: "Downloads"),
+        ),
+        body: ListView.separated(
+            padding: const EdgeInsets.all(10),
+            itemBuilder: (ctx, index) => _widgetList[index],
+            separatorBuilder: (ctx, index) => const SizedBox(height: 25),
+            itemCount: _widgetList.length));
+  }
+}
+
+class Section2 extends StatelessWidget {
+  const Section2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    BlocProvider.of<DownloadsBloc>(context)
+        .add(const DownloadsEvent.getDownloadsImage());
+    final Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        const Text(
+          "Introducing Downloads for you",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: kwhitecolor,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        kheight,
+        const Text(
+          "We will download a personalised selection of movies and shows for you,so there is\n always there is something on your\n device",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.grey, fontSize: 16),
+        ),
+        BlocBuilder<DownloadsBloc, DownloadsState>(
+          builder: (context, state) {
+            return SizedBox(
+              width: size.width,
+              height: size.width,
+              child: state.isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: size.width * 0.37,
+                          backgroundColor: Colors.grey.withOpacity(0.5),
+                        ),
+                        DownloadsImageWidget(
+                          imageList:
+                              '$imageAppendUrl${state.downloads[0].posterPath}',
+                          margin: const EdgeInsets.only(left: 160, top: 15),
+                          angle: 15,
+                          size: Size(size.width * 0.35, size.width * 0.55),
+                        ),
+                        DownloadsImageWidget(
+                          imageList:
+                              '$imageAppendUrl${state.downloads[1].posterPath}',
+                          margin: const EdgeInsets.only(right: 160, top: 15),
+                          angle: -15,
+                          size: Size(size.width * 0.35, size.width * 0.55),
+                        ),
+                        DownloadsImageWidget(
+                          imageList:
+                              '$imageAppendUrl${state.downloads[2].posterPath}',
+                          margin: const EdgeInsets.only(top: 5),
+                          size: Size(size.width * 0.39, size.height * 0.28),
+                        ),
+                      ],
+                    ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class Section3 extends StatelessWidget {
+  const Section3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: MaterialButton(
+            color: Colors.blue,
+            onPressed: () {},
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: Text(
+                'Setup',
+                style: TextStyle(
+                    color: kwhitecolor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+        kheight,
+        SizedBox(
+          width: double.infinity,
+          child: MaterialButton(
+            color: kbuttonColorWhite,
+            onPressed: () {},
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                "See what you can download",
+                style: TextStyle(
+                    color: kblackcolor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _SmartDownloads extends StatelessWidget {
+  const _SmartDownloads({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        kwidth,
+        Icon(
+          Icons.settings,
+          color: Colors.white,
+        ),
+        kwidth,
+        Text('Smart Downloads'),
+      ],
+    );
+  }
+}
+
+class DownloadsImageWidget extends StatelessWidget {
+  const DownloadsImageWidget({
+    super.key,
+    required this.imageList,
+    this.angle = 0,
+    required this.size,
+    required this.margin,
+    this.radius = 10,
+  });
+
+  final String imageList;
+  final double angle;
+  final EdgeInsets margin;
+  final Size size;
+  final double radius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin,
+      child: Transform.rotate(
+        angle: angle * pi / 180,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(radius),
+          child: Container(
+            width: size.width,
+            height: size.height,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(imageList),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
